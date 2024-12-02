a09f66f1c3bb6c18ff25becfe5a155a1b9d2fb01
Vedant Singh <v1singh@bravurasolutions.com>
2024-12-03 00:29:30 +0530

    schedule controller changes

--- src/main/java/com/shadow/controller/ScheduleController.java
+++ src/main/java/com/shadow/controller/ScheduleController.java
@@ -2,6 +2,7 @@ package com.shadow.controller;
 
 import com.shadow.entity.Schedule;
 import com.shadow.misc.CronExpressions;
+import com.shadow.repository.ClientRepository;
 import com.shadow.service.ScheduleService;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.*;
@@ -26,11 +27,32 @@ public class ScheduleController {
             case "every_5_minutes":
                 schedule.setCronExpression(CronExpressions.EVERY_5_MINUTES);
                 break;
+            case "every_10_minutes":
+                schedule.setCronExpression(CronExpressions.EVERY_10_MINUTES);
+                break;
+            case "every_15_minutes":
+                schedule.setCronExpression(CronExpressions.EVERY_15_MINUTES);
+                break;
+            case "every_30_minutes":
+                schedule.setCronExpression(CronExpressions.EVERY_30_MINUTES);
+                break;
             case "every_hour":
                 schedule.setCronExpression(CronExpressions.EVERY_HOUR);
                 break;
+            case "every_6_hours":
+                schedule.setCronExpression(CronExpressions.EVERY_6_HOURS);
+                break;
+            case "every_12_hours":
+                schedule.setCronExpression(CronExpressions.EVERY_12_HOURS);
+                break;
             case "daily":
                 schedule.setCronExpression(CronExpressions.DAILY);
+                break;
+            case "weekly":
+                schedule.setCronExpression(CronExpressions.WEEKLY);
+                break;
+            case "monthly":
+                schedule.setCronExpression(CronExpressions.MONTHLY);
                 break;
             default:
                 throw new IllegalArgumentException("Invalid interval: " + interval);
